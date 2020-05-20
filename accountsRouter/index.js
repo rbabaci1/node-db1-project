a6072ef4109022765c5638e3db0c8ca895cbc524
rabah babaci <4rabah@gmail.com>
2020-05-20 13:33:10 -0700

    implement the get all accounts endpoint

--- accountsRouter/index.js
+++ accountsRouter/index.js
@@ -4,8 +4,14 @@ const db = require("../data/dbConfig");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "API is up!" });
+router.get("/", async (req, res) => {
+  try {
+    const accounts = await db("accounts");
+
+    res.status(200).json(accounts);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -7,4 +7,8 @@ const server = express();
 server.use(express.json());
 server.use("/api/accounts", accountsRouter);
 
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "API is up!" });
+});
+
 module.exports = server;
