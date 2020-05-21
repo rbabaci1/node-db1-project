dc242bafd4d3d6b499f115a2a8dab8ef24604bb8
rabah babaci <4rabah@gmail.com>
2020-05-20 17:25:48 -0700

    create the ID and body validation middleware

--- accountsRouter/index.js
+++ accountsRouter/index.js
@@ -17,4 +17,42 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {});
+
+////////////////////////////////////////
+async function validateId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const account = await getById(id);
+
+    if (account) {
+      next();
+    } else {
+      next({
+        status: 404,
+        message: `The account with the specified ID does not exist.`,
+      });
+    }
+  } catch (err) {
+    next({
+      error: `The account info could not be retrieved at this moment.`,
+      reason: err.message,
+    });
+  }
+}
+
+function validateBody(req, res, next) {
+  const { body } = req;
+
+  if (typeof body === undefined) {
+    res.status(400).json({ message: `Request body is missing.` });
+  } else if (!body.hasOwnProperty("name") && !body.hasOwnProperty("text")) {
+    res.status(400).json({
+      message: `Some info in the body is missing or incorrectly defined.`,
+    });
+  } else {
+    next();
+  }
+}
+
 module.exports = router;
